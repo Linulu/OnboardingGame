448b9e1c6b4cc29b6eb48780f8b93077b00cdc36
Linus <lcg@telia.com>
2020-10-06 16:55:04 +0200

    Started TaskContent page
    Started to create the rought scetch of the TaskContent page meant to be implemented insted of the AlertDisplay warnings in TaskPage

---
+++ OnboardingGame/OnboardingGame/OnboardingGame/Pages/TaskContent.xaml.cs
@@ -0,0 +1,40 @@
+﻿using OnboardingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OnboardingGame.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskContent : ContentPage
+    {
+        public TaskContent()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var task = (TaskItem)BindingContext;
+        }
+
+        private Span CreateSpan(TaskItem context) {
+
+            var span = new Span();
+
+            span.GestureRecognizers.Add(new TapGestureRecognizer() {
+                //Command = navigationCommand
+                //CommandParameter = context.link
+            });
+
+            return span;
+        }
+    }
+}
